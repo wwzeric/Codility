fbb082e9cc20c612ba6ea5e1e28f332304ee7c9f
baitiansaixinxin <936316528@qq.com>
2023-04-04 12:35:53 +0200

    Create 1_CountFactors.py

---
+++ Lesson 10/1_CountFactors.py
@@ -0,0 +1,13 @@
+# A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
+
+# For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
+
+def solution(N):
+  i=2
+  count=2
+  while i*i<N:
+    if N%i==0:
+      count+=2
+  if i*i==N:
+    count+=1
+  return count
